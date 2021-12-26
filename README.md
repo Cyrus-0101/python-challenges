550f8678e05eaaed6902520d1f64fbd1beb5aa0e
Toxic254 <cyrus_g@outlook.com>
2021-12-26 17:25:35 +0300

    Worked on a credit card masking challenge.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python CHallenges
+
+* A number of Python challenges I practice on as I explore coding. Some challenges are found online, others I just think of them.
---
+++ credit-card-masking.py
@@ -0,0 +1,59 @@
+"""
+    Write a new function that passes in a credit card number
+    and returns a masked number. Only show the last 4 digits of
+    the credit card number
+
+    Notes/Constraints:
+
+    * Cards may have different amount of numbers. 
+
+"""
+# Sol 1
+
+## Step 1
+
+credit_card_str = "1234123412341234"
+
+string_length = len(credit_card_str)
+
+mask = string_length - 4
+
+show_str = credit_card_str[mask:]
+
+print(("*" * mask) + show_str)
+
+
+## Step 2
+
+def masking(credit_card_number, numbers_to_mask):
+    
+    nums_to_mask = ''
+
+    n = numbers_to_mask
+
+    for i in credit_card_number[:-n]:
+        
+        nums_to_mask += '*'
+
+    nums_to_mask += credit_card_number[-n:]
+
+    return nums_to_mask
+
+print(masking("1234123412341234", 4))
+
+# Sol 2
+
+credit_card_str = "1234123412341234"
+
+masked_number = credit_card_str[-4:].rjust(len(credit_card_str), '*')
+
+print(masked_number)
+
+
+# Sol 3 (Hacky)
+
+credit_card_str = "12341234123412345678"
+
+masked_number = '*' * 12 + credit_card_str[-4:]
+
+print(masked_number)
